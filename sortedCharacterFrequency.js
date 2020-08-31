1b1c3e9cd8212e68f9efc32387a0d0030187dc42
Jeremy Gray <jeremytgray6@gmail.com>
2020-08-31 15:25:45 -0500

    solved sorting char freq problem

--- sortedCharacterFrequency.js
+++ sortedCharacterFrequency.js
@@ -7,3 +7,48 @@
 // "mississippi" => [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
 // "" => [ ]
 //can be lower or upper case
+
+//pseudocode
+//lower case the string, split into array of chars
+//create empty counts = [] unsorted at this point
+//loop thru char array and do find on counts where count[0]===char
+//if not found, create letterCount = [], letterCount[0]=char, letterCount[1]=1, push letterCount into counts
+//if found, foundChar[1]+=1
+//reverse sort counts by count[1] - sort function (a,b)=>a-b
+//sort by count[0]
+//return counts; sorted at this point
+
+function characterFrequency (str) {
+    const lowerStr = str.toLowerCase();
+    const strArr = lowerStr.split('');
+    const counts = [];
+    strArr.forEach(char => {
+      const foundChar = counts.find(count => {
+        return count[0]===char;
+      });
+      if(foundChar){
+        foundChar[1]+=1;
+      } else {
+        const letterCount = [];
+        letterCount[0]=char;
+        letterCount[1]=1;
+        counts.push(letterCount);
+      };
+    });
+    counts.sort((a,b) => {
+      if(a[1]>b[1]){
+        return -1
+      } else if (a[1]<b[1]){
+        return 1
+      } else if (a[0]<b[0]){
+        return -1
+      } else if (a[0]>b[0]){
+        return 1
+      }
+      return 0
+    });
+    
+    return counts
+  };
+  
+  console.log(characterFrequency("mississippi"));
