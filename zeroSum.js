dc2ddcbb99c90b8cdeda5efa9ffc7dbbe2601486
Jeremy Gray <jeremytgray6@gmail.com>
2020-09-09 12:18:42 -0500

    added pseduocode for first solution to zeroSum problem

--- zeroSum.js
+++ zeroSum.js
@@ -3,6 +3,12 @@
 // Challenge: see if you can solve it with linear time complexity after your initial solution (which will most likely be quadratic); if you get linear on the first round then good job!
 
 function zeroSum(nums){
+    //needs to have two numbers that add up to zero, so length of nums should be greater than or equal to 2, otherwise return false
+    //we want to loop through the nums array twice (outer for loop and inner for loop):
+    //first loop (outer) starts at first element and will increment until reaching the second to last element
+    //second loop (inner) starts at second element and will increment until reaching the last element
+    //when the sum of elements from nums at the two for loop iteration indices equals zero, return true
+    //if it makes it through both for loops (a.k.a. didn't find two elements that sum to zero), return false
     if (nums.length >= 2){
       for (let i=0; i<nums.length-1; i++){
         for (let j=1; j<nums.length; j++){
