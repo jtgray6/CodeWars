14557ab749a31c8675de1f2775ce1a812d2686a4
Jeremy Gray <jeremytgray6@gmail.com>
2020-09-11 14:13:14 -0500

    new code challenge for returing largest product of three integers from a given array

---
+++ largestProdOf3.js
@@ -0,0 +1,10 @@
+//given an array of integers, return the largest product of any 3 of the integers
+
+// [2, 1, 3, 7]
+// [0, 2, 3]
+// [-5, -1, -3, -2, -4]
+// [-31, 41, 34, -37, -17, 29]
+// 42
+// 0
+// -6
+// 47027
