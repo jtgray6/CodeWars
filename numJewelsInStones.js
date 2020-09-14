c1f9a9639d3f3a06c6cbbebb9dc0358ecec038e5
Jeremy Gray <jeremytgray6@gmail.com>
2020-09-13 21:50:22 -0500

    code challenge for returning number of characters from one string occurring in a second string

---
+++ numJewelsInStones.js
@@ -0,0 +1,7 @@
+// Num Jewels in Stones:
+
+// Given two strings as your input (with upper and lower case alphabetic characters), return a number indicating how many times a character from string 1 occurs in string 2
+
+// @param {string} J
+// @param {string} S
+// @return {number}
