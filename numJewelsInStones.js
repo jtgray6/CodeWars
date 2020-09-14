8f26bde89b3ec3fee9b0651f7854b4bf7d7be1ec
Jeremy Gray <jeremytgray6@gmail.com>
2020-09-13 21:51:52 -0500

    solved number of jewels in stones code challenge with quadratic time complexity

--- numJewelsInStones.js
+++ numJewelsInStones.js
@@ -5,3 +5,25 @@
 // @param {string} J
 // @param {string} S
 // @return {number}
+
+var numJewelsInStones = function(J, S) {
+    //initalize a count that starts at 0  
+    //outer for loop (i) through str1 and,
+    //  inner for loop (j) through str2
+    //    if str1[i] equals str2[j], increase count by 1
+    //exit loops and then return count
+    let count = 0;
+    for (let i=0; i<J.length; i++){
+        for (let j=0; j<S.length; j++){
+            if (J[i] === S[j]){
+                count += 1;
+            }
+        }
+    }
+    return count;
+  };
+  
+  
+  console.log(numJewelsInStones('aA', 'aAAbbb')); // 3
+  console.log(numJewelsInStones('z', 'ZZ')); // 0
+  console.log(numJewelsInStones('bZw', 'BbzZZwW')); // 4
