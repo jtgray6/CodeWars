2026cf6bad00c96bc5bd532e241ab8aefcc2b121
Jeremy Gray <jeremytgray6@gmail.com>
2020-09-15 22:19:47 -0500

    solved fizzbuzz code challenge

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -24,3 +24,31 @@
 //     "14",
 //     "FizzBuzz"
 // ]
+
+//time complexity O(n) -> linear
+
+const fizzBuzz = (num) => {
+//declare an empty result array
+//loop num iterations and for each iteration:
+// if remainder of iteration / 15 is zero, then set result[i]="FizzBuzz"
+// if iteration modulus 5 is zero, then set result[i]="Buzz"
+// if iteration modulus 3 is zero, then set result[i]="Fizz"
+// else result[i]=`${i}`
+//exits loop, return result
+  const result = [];
+  for (let i=1; i<=num; i++){
+    if (i % 15 === 0){
+      result[i-1]="FizzBuzz";
+    } else if (i % 5 === 0){
+      result[i-1]="Buzz";
+    } else if (i % 3 === 0){
+      result[i-1]="Fizz";
+    } else {
+      result[i-1]=`${i}`;
+    }
+  }
+  return result;
+}
+
+console.log(fizzBuzz(0)); // []
+console.log(fizzBuzz(45)); // []
